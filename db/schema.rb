db93fc66113382734f33d3060a3aa8d77adc4850
Laura Messa Travin <laura.travin@gmail.com>
2021-03-18 23:34:56 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20210318232623) do
+ActiveRecord::Schema.define(version: 20210318233442) do
 
   create_table "authors", force: :cascade do |t|
     t.string   "name"
@@ -29,10 +29,10 @@ ActiveRecord::Schema.define(version: 20210318232623) do
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "description"
-    t.datetime "created_at",       null: false
-    t.datetime "updated_at",       null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "category_id"
-    t.string   "published_status"
+    t.boolean  "post_status"
   end
 
 end
